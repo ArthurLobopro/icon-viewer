6a6c32a0dfb7f107e2f9035db05c1063c6dac396
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-07-29 23:25:25 -0300

    webcontent archives

---
+++ public/index.js
@@ -0,0 +1,26 @@
+window.onload = () => {
+
+    let isVisible = false
+
+    let atualPath
+
+    const addImage = document.getElementById('add')
+
+    addImage.onclick = async () => {
+        const [imagePath] = await ipcRenderer.invoke('add-file')
+        ipcRenderer.send('change-icon', imagePath)
+        if (imagePath !== false) {
+            atualPath = imagePath
+            if (!isVisible) {
+                const wrapper = document.querySelectorAll('.img-wrapper')[1]
+                wrapper.style.display = ""
+                isVisible = true
+            }
+        }
+    }
+
+    const attImage = document.getElementById('att')
+
+    attImage.onclick = () => ipcRenderer.send('change-icon', atualPath)
+
+}
