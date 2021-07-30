8c7470f9f17c0b0c1564d2ee80c967b373e1fc3c
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-07-29 23:24:13 -0300

    Create main.js

---
+++ electron/main.js
@@ -0,0 +1,53 @@
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+const path = require('path');
+
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    autoHideMenuBar: true,
+    webPreferences:{
+      nodeIntegration: true,
+      preload: path.resolve(__dirname, "preload.js")
+    }
+  });
+
+  mainWindow.loadFile(path.join(__dirname, '../public/index.html'));
+
+  // mainWindow.webContents.openDevTools();
+};
+
+app.on('ready', createWindow);
+
+ipcMain.handle('add-file', async (event, arg) => {
+  const sysIcon = process.platform === 'win32' ? 'ico' : null
+  return dialog.showOpenDialog({ 
+      properties: ['openFile'],
+      filters: [{ name: 'Imagens', extensions: ['jpg', 'png', 'webp', 'jpeg', sysIcon]}]
+  })
+  .then( res => {
+      return res.canceled === true ? false : res.filePaths
+  })
+})
+
+ipcMain.on('change-icon', (event,arg) => {
+  const window = BrowserWindow.getFocusedWindow()
+  console.log(arg);
+  window.setIcon(arg)
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
